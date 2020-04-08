f891fcfb8427b5508b20334cca7da1fbdc5e6f4c
Ricardo Juarez <kronos_20_11@hotmail.com>
2020-04-08 16:54:08 -0500

    importado el signin

--- puma_home/lib/main.dart
+++ puma_home/lib/main.dart
@@ -1,10 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:puma_home/misgrupos.dart';
 import 'package:puma_home/signin.dart';
 
 void main() {
   runApp(
     MaterialApp(
-      home: Signin(),
+      //home: Signin(),
+      home: MisGrupos(),
     ),
   );
 }
