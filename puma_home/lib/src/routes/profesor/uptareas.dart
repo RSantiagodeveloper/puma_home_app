5b7f7a32285dd16a6e44c6fa4f9456dbc6c4d619
kirtashGuerrero27 <chivas474@hotmail.com>
2020-06-16 17:29:29 -0500

    16/06/2020 semerge de ramas sobre la master

--- puma_home/lib/src/routes/profesor/uptareas.dart
+++ puma_home/lib/src/routes/profesor/uptareas.dart
@@ -5,7 +5,7 @@ import 'package:firebase_storage/firebase_storage.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:file_picker/file_picker.dart';
 import 'package:flutter/services.dart'; //import del PlatformException
-import 'package:puma_home/src/resources/App_Elements.dart';
+import 'package:puma_home/src/resources/App_Elements.dart'; 
 
 
 class SubirArchivo extends StatefulWidget{
